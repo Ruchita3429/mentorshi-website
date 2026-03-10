cb36232260d6ccc3b123bdffda989e048c129407
Ruchita <mhatreruchita03@gmail.com>
2026-03-11 00:00:24 +0530

    fix: deployment url fix

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,10 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'http://localhost:3001'],
+    origin: ['http://localhost:3000',
+       'http://localhost:3001', 
+      'https://mentorship-platform.vercel.app',
+    ],
     credentials: true,
   })
 );
